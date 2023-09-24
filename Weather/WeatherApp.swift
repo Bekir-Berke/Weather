f44025b6844377988a294d5167c39a5e045b4a69
Bekir Berke <76753313+Bekir-Berke@users.noreply.github.com>
2023-09-24 12:17:36 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Bekir Berke Yılmaz on 24.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
